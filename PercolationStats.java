d53df197ebbabc06dc49f2ad389c7830c16f87c9
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-04-24 17:02:43 +1000

    Initial commit for percolation stats class

---
+++ PercolationStats.java
@@ -0,0 +1,20 @@
+public class PercolationStats {
+    
+   // perform trials independent experiments on an n-by-n grid
+   public PercolationStats(int n, int trials)
+       
+   // sample mean of percolation threshold
+   public double mean()
+       
+   // sample standard deviation of percolation threshold
+   public double stddev()
+       
+   // low  endpoint of 95% confidence interval    
+   public double confidenceLo()
+       
+   // high endpoint of 95% confidence interval
+   public double confidenceHi() 
+       
+   // test client
+   public static void main(String[] args)         
+}
