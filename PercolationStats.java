acb9916d265884dfe2731ae2b28bc4ce9a674070
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-04-27 16:25:34 +1000

    Initial commit for Percolation Stats class

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,20 +1,40 @@
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
     
+    StdStats stats;
+    StdRandom random;
+    Percolation percolationTrial;
+    
    // perform trials independent experiments on an n-by-n grid
-   public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials) {
+        double[] openSitesAtPercolation = new double[trials];
+        for(int i = 0; i < trials; i++) {
+            percolationTrial = new Percolation(n);
+//            while( !percolationTrial.percolates() ) {
+//                double randomRow =
+//                double randomCol =
+//            }
+
+            // randomly open site
+            // check for percolation, break if true
+            // return number of open sites
+        }
+    }
        
    // sample mean of percolation threshold
-   public double mean()
-       
-   // sample standard deviation of percolation threshold
-   public double stddev()
-       
-   // low  endpoint of 95% confidence interval    
-   public double confidenceLo()
-       
-   // high endpoint of 95% confidence interval
-   public double confidenceHi() 
-       
-   // test client
-   public static void main(String[] args)         
+//    public double mean() {}
+//       
+//   // sample standard deviation of percolation threshold
+//    public double stddev() {}
+//       
+//   // low  endpoint of 95% confidence interval    
+//    public double confidenceLo() {}
+//       
+//   // high endpoint of 95% confidence interval
+//    public double confidenceHi() {}
+//       
+//   // test client
+//    public static void main(String[] args) {}     
 }
