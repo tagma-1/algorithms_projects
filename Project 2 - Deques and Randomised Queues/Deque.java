ebbd0b4e660e9df72fd14d51109f8a8baa12a831
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-05-11 16:09:22 +1000

    Add methods to add items to the front and back of the deque

--- Project 2 - Deques and Randomised Queues/Deque.java
+++ Project 2 - Deques and Randomised Queues/Deque.java
@@ -1,13 +1,17 @@
 public class Deque<Item> implements Iterable<Item> {
    
-   private Item first; 
-   private Item last;
+   private Node first; 
+   private Node last;
    private int n; // size of the deque
    
    // sub class to create nodes for a linked list
    private class Node {
        private Item item;
-       private Item next;
+       private Node next;
+       public Node(Item nodeItem, Node nextNode) {
+           item = nodeItem;
+           next = nextNode;
+       }
    }
    
    // construct an empty deque
@@ -27,8 +31,39 @@ public class Deque<Item> implements Iterable<Item> {
        return n;
    }
    
-   public void addFirst(Item item)          // add the item to the front
-   public void addLast(Item item)           // add the item to the end
+   // add the item to the front
+   public void addFirst(Item item) {
+       if (n == 0) {
+           return addInitial(item);
+       } else {
+           Node oldFirst = first;
+           newNode = new Node(item, oldFirst);
+           first = newNode;
+           n++;
+       }
+   }
+   
+   // add the item to the end
+   public void addLast(Item item) {
+       if (n == 0) {
+           return addInitial(item);
+       } else {
+           Node oldLast = last;
+           newNode = new Node(item, null);
+           oldLast.next = newNode;
+           last = newNode;
+           n++;
+       }
+   }
+       
+   // add the first item to the deque
+   private void addInitial(Item item) {
+       newNode = new Node(item, null);
+       first = newNode;
+       last = newNode;
+       n++;
+   }
+   
    public Item removeFirst()                // remove and return the item from the front
    public Item removeLast()                 // remove and return the item from the end
    public Iterator<Item> iterator()         // return an iterator over items in order from front to end
