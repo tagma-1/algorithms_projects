2a7e6faa3ad30ddc4dae52bb49f66c21aed81911
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-05-15 14:25:13 +1000

    Add client program

--- Project 2 - Deques and Randomised Queues/Permutation.java
+++ Project 2 - Deques and Randomised Queues/Permutation.java
@@ -1,3 +1,27 @@
+import edu.princeton.cs.algs4.StdIn; 
+
 public class Permutation {
-   public static void main(String[] args)
+   
+   /*
+    takes an integer k as a command-line argument; 
+    reads in a sequence of strings from standard input; and 
+    prints exactly k of them, uniformly at random.   
+    */
+   
+    
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<String>(); 
+        
+        while (!StdIn.isEmpty()) {
+             queue.enqueue(StdIn.readString());
+        }
+        
+        for(String item : queue) {
+            if (k > 0) {
+                System.out.println(item);
+                k--;
+            }
+        }
+    }
 }
