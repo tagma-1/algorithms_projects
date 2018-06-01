adafb6d75e4c318f9975350f45d759c87256b766
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-06-01 16:51:58 +1000

    Add corner case exceptions

--- Project 3 - Pattern Recognition/BruteCollinearPoints.java
+++ Project 3 - Pattern Recognition/BruteCollinearPoints.java
@@ -7,28 +7,35 @@ public class BruteCollinearPoints {
    
    // finds all line segments containing 4 points
    public BruteCollinearPoints(Point[] points) {
-       int n = points.length;
+  
+       if (points == null) throw new IllegalArgumentException("Array of points must be provided.");
+       int n = points.length; 
        
+       // check for null points prior to sorting
+       for (int i = 0; i < n; i++) {
+           if (points[i] == null) throw new IllegalArgumentException("Array at index " + i + " is null.");
+       }  
+                  
        // initialise an array with a length equal to the maximum number of possible line segments 
        lineSegments = new LineSegment[n / 4];
        
-       // sort the array by y-coordinates (natural order)
+       // sort the array by y-coordinates (natural order) then check for repeated points
        Arrays.sort(points, 0, n);
-       
+       for (int i = 0; i < n; i++) {
+           if (i > 0 && points[i] == points[i - 1]) throw new IllegalArgumentException("Array contains repeated points.");
+       } 
+
        // iterate through the array 4 times to find possible combinations (not permutations)
        for (int i = 0; i < n - 3; i++) {
            
            for (int j = i + 1; j < n - 2; j++) {
-               
                double secondPointSlope = points[i].slopeTo(points[j]);
                
-               for (int k = j + 1; k < n - 1; k++) {
-                   
+               for (int k = j + 1; k < n - 1; k++) { 
                    double thirdPointSlope = points[i].slopeTo(points[k]);
                    if (secondPointSlope != thirdPointSlope) continue; // skip the fourth loop if the current three points are not collinear
                    
                    for (int l = k + 1; l < n; l++) {
-                       
                        double fourthPointSlope = points[i].slopeTo(points[l]);
                        if (fourthPointSlope == secondPointSlope) {
                            segmentCounter++;
@@ -44,8 +51,6 @@ public class BruteCollinearPoints {
     
    public int numberOfSegments() { return segmentCounter; }
    
-   public LineSegment[] segments() {
-       return lineSegments;
-   }
+   public LineSegment[] segments() { return lineSegments; }
    
 }
