894300da394b650655d299ba9e60b2333f99a67f
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-06-01 15:43:25 +1000

    Add brute force aglorithm to check for and return collinear points

--- Project 3 - Pattern Recognition/BruteCollinearPoints.java
+++ Project 3 - Pattern Recognition/BruteCollinearPoints.java
@@ -1,5 +1,51 @@
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
-   public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
-   public           int numberOfSegments()        // the number of line segments
-   public LineSegment[] segments()                // the line segments
+   
+   private int segmentCounter = 0;
+   private LineSegment[] lineSegments;
+   
+   // finds all line segments containing 4 points
+   public BruteCollinearPoints(Point[] points) {
+       int n = points.length;
+       
+       // initialise an array with a length equal to the maximum number of possible line segments 
+       lineSegments = new LineSegment[n / 4];
+       
+       // sort the array by y-coordinates (natural order)
+       Arrays.sort(points, 0, n);
+       
+       // iterate through the array 4 times to find possible combinations (not permutations)
+       for (int i = 0; i < n - 3; i++) {
+           
+           for (int j = i + 1; j < n - 2; j++) {
+               
+               double secondPointSlope = points[i].slopeTo(points[j]);
+               
+               for (int k = j + 1; k < n - 1; k++) {
+                   
+                   double thirdPointSlope = points[i].slopeTo(points[k]);
+                   if (secondPointSlope != thirdPointSlope) continue; // skip the fourth loop if the current three points are not collinear
+                   
+                   for (int l = k + 1; l < n; l++) {
+                       
+                       double fourthPointSlope = points[i].slopeTo(points[l]);
+                       if (fourthPointSlope == secondPointSlope) {
+                           segmentCounter++;
+                           Point minPoint = points[i];
+                           Point maxPoint = points[l];
+                           lineSegments[segmentCounter - 1] = new LineSegment(minPoint, maxPoint);
+                       }
+                   }
+               }
+           }
+       }
+   }
+    
+   public int numberOfSegments() { return segmentCounter; }
+   
+   public LineSegment[] segments() {
+       return lineSegments;
+   }
+   
 }
